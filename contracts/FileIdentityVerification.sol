f452958988976c5df864859e05df4ac0faf086c6
Osamu-Sawada <11325088+Osamu-Sawada@users.noreply.github.com>
2019-04-09 11:41:26 +0900

    Modify comments and variables

--- contracts/FileIdentityVerification.sol
+++ contracts/FileIdentityVerification.sol
@@ -25,16 +25,10 @@ contract FileIdentityVerification {
         uint isExist;
     }
 
-    // Cumulative number of hashes registered
-    // ハッシュを登録した累計回数
-    uint public number_of_hashes_registered;
-
     // Number of valid hashes
-    // 有効なハッシュの数
-    uint public number_of_valid_hashes;
+    uint public number_of_registrations;
 
     // Keep a list of hashes
-    // ハッシュの一覧を保持する
     mapping(bytes32 => FileIdentity) private fileIdentityList;
 
     string private NO_DATA = 'Data does not exist';
@@ -45,22 +39,20 @@ contract FileIdentityVerification {
     event FileHash(address indexed _from, bytes32 _fileId, bytes _md5, bytes _sha256, bytes _sha512, uint _timestamp);
 
     constructor() public {
-        number_of_hashes_registered = 0;
-        number_of_valid_hashes = 0;
+        number_of_registrations = 0;
     }
 
-    // ファイル情報を照会
+    // @title Get file information
     function getFileIdentity(bytes32 fileId) public view
     returns (bytes32 _fileId, bytes memory _md5, bytes memory _sha256, bytes memory _sha512, address _registrant, uint _timestamp, uint _isExist){
         FileIdentity memory fi = fileIdentityList[fileId];
         return (fi.fileId, fi.md5, fi.sha256, fi.sha512, fi.registrant, fi.timestamp, fi.isExist);
     }
 
-    // File existence check
-    // ファイルの存在チェック
+    // @title File existence check
     function isExist(bytes32 fileId) public view returns (bool) {
         if (fileIdentityList[fileId].isExist == 1) {
-            // exist
+            // isExist == 1 is mean : exist
             return true;
         }
         else {
@@ -69,12 +61,12 @@ contract FileIdentityVerification {
         }
     }
 
+    // @title Calculate file ID from file hash
     function getFileId(string memory _md5, string memory _sha256, string memory _sha512) public pure returns (bytes32) {
         return keccak256(abi.encodePacked(bytes(_md5), bytes(_sha256), bytes(_sha512)));
     }
 
-    // Register file hash
-    // ファイルハッシュを登録する
+    // @title  Register file hash
     function registerFileHash(string memory _md5, string memory _sha256, string memory _sha512) public returns (bool) {
 
         bytes32 fileId = getFileId(_md5, _sha256, _sha512);
@@ -96,12 +88,7 @@ contract FileIdentityVerification {
         emit FileHash(msg.sender, fileId, bytes(_md5), bytes(_sha256), bytes(_sha512), ts);
 
         // Add actual registration number
-        // 実際の登録数
-        ++number_of_valid_hashes;
-
-        // Add cumulative number
-        // 累計数
-        ++number_of_hashes_registered;
+        ++number_of_registrations;
 
         return true;
     }
