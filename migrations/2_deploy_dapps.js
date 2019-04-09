e3b17f6ee83de7012a501d91599a5395dcb3919f
Osamu-Sawada <11325088+Osamu-Sawada@users.noreply.github.com>
2019-04-09 11:47:05 +0900

    Fix migration script, add ABI File

--- migrations/2_deploy_dapps.js
+++ migrations/2_deploy_dapps.js
@@ -1,5 +1,10 @@
+const fs = require('fs');
 const FileIdentityVerification = artifacts.require('./FileIdentityVerification.sol');
 
 module.exports = (deployer) => {
-    deployer.deploy(FileIdentityVerification);
+    deployer.deploy(FileIdentityVerification).then(() => {
+        // Save ABI to file
+        fs.mkdirSync('deploy/abi/', { recursive: true });
+        fs.writeFileSync('deploy/abi/FileIdentityVerification.json', JSON.stringify(FileIdentityVerification.abi), { flag: 'w' });
+    });
 };
